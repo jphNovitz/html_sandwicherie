9ee6cb5ab8bc832893cd71aa26024196230be4ec
Novitz Jean-Philippe <novitz@gmail.com>
2021-11-06 23:37:52 +0100

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        "rusty": ['Rusty Cola Pen'],
+        "rusty-all-caps": ['Rusty Cola Pen Allcaps']
+      },
       backgroundColor: {
         // 'hero': "rgba(0, 0, 0, 1)"
       },
       backgroundImage: {
         'hero': "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3) ), url('../public/images/raphael-nogueira-Znvxeud6sDc-unsplash.jpg')",
-        'prom': "linear-gradient(to right, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.6),  rgba(0, 0, 0, 0) )"
+        'prom': "linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.6),  rgba(0, 0, 0, 0) )"
         // 'prom': "linear-gradient(to right, rgba(217, 219, 6, 0), rgba(217, 219, 6, 0.6),  rgba(217, 219, 6, 0) )"
       }
     },
