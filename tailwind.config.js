25c38a012a56a20ead20dbdd6cc1bda2bcce78de
Novitz Jean-Philippe <novitz@gmail.com>
2021-11-10 05:27:29 +0100

    Menu part - start

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,18 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        backgroundSize: {
+            'auto': 'auto',
+            'cover': 'cover',
+            'contain': 'contain',
+            '80%': '80%',
+            '50%': '50%',
+            '40%': '40%',
+            '33%': '33%',
+        },
         colors: {
             "primary": "#86bc42",
-            "dark": "#d2611",
+            "dark": "#0d2611",
             black: colors.black,
             white: colors.white,
             gray: colors.coolGray,
@@ -27,7 +36,8 @@ module.exports = {
             },
             backgroundImage: {
                 'hero': "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3) ), url('../public/images/raphael-nogueira-Znvxeud6sDc-unsplash.jpg')",
-                'prom': "linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4) )",
+                'prom': "linear-gradient(to right, rgba(134, 188, 66, 0), rgba(134, 188, 66, 0.4), rgba(134, 188, 66, 0.6) )",
+                // 'prom': "linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6) )",
                 // 'prom': "linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.6),  rgba(0, 0, 0, 0) )",
                 'roundies': "url('../public/images/bg-rounds.png')"
                 // 'prom': "linear-gradient(to right, rgba(217, 219, 6, 0), rgba(217, 219, 6, 0.6),  rgba(217, 219, 6, 0) )"
